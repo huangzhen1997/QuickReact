b806c8d0b39be87a5a63643304d23078c31f5eb0
Zhen Huang <zhuang38@hawk.iit.edu>
2020-04-11 10:36:40 -0500

    filter by term

--- src/App.js
+++ src/App.js
@@ -21,12 +21,37 @@ const Course = ({ course }) => (
     { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
   </button>
 );
-  
-const CourseList = ({ courses }) => (
-  <Button.Group>
-    { courses.map(course => <Course course={ course } />) }
+
+const buttonColor = selected => (
+  selected ? 'success' : null
+);
+
+const TermSelector=({state})=>(
+  <Button.Group hasAddons>
+    {
+      Object.values(terms).map(value=>
+      <Button key={value}
+       color={buttonColor(value===state.term)}
+       onClick={()=>state.setTerm(value)}>
+        {value}
+      </Button>)
+    }
   </Button.Group>
 );
+
+const CourseList = ({ courses }) => {
+  const [term,setTerm] = useState("Fall");
+  const termCourses = courses.filter(course=>term === getCourseTerm(course));
+
+  return(
+    <React.Fragment>
+    <TermSelector state = {{term,setTerm}}/>
+    <Button.Group>
+      { termCourses.map(course => <Course course={ course } key={course.id}/>) }
+    </Button.Group>
+    </React.Fragment>
+  );
+};
 
 const App = () => {
   const [schedule, setSchedule] = useState({title:'',courses:[]});
